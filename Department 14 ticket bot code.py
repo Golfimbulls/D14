f2402113c31020b5b7f24158fc642c0527aa0ab0
Golfimbulls <150305582+Golfimbulls@users.noreply.github.com>
2023-11-09 16:28:35 -0800

    Update Department 14 ticket bot code.py

--- Department 14 ticket bot code.py
+++ Department 14 ticket bot code.py
@@ -16,3 +16,21 @@ async def open_ticket(ctx):
         # Log the ticket creation in the log channel
         if logging_states['message_edit']:
             await log_channel.send(f"Ticket opened by {ctx.author.display_name}. Channel: {channel.name}")
+
+@bot.command(name='closeticket', help='Close your current support ticket.')
+async def close_ticket(ctx):
+    if ctx.author.id in tickets:
+        # Get the ticket channel
+        channel = bot.get_channel(tickets[ctx.author.id])
+
+        # Delete the ticket channel
+        await channel.delete()
+
+        # Remove the ticket from the dictionary
+        del tickets[ctx.author.id]
+
+        # Log the ticket closure in the log channel
+        if logging_states['message_edit']:
+            await log_channel.send(f"Ticket closed by {ctx.author.display_name}. Channel: {channel.name}")
+    else:
+        await ctx.send("You don't have an open ticket.") 
